817b8475b063ebcdaa3f5ac6a5d9170dafb6bc5b
James Rodden <james.rodden@savanta.com>
2021-10-08 14:28:22 +0100

    Add perspective bar behind the rest of the content

---
+++ src/components/PerspectiveBar/PerspectiveBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './PerspectiveBar.module.css';
+
+function PerspectiveBar() {
+
+    return (
+        <div className={styles.perspectiveBar} />
+    )
+}
+
+export default PerspectiveBar;
